09814519478c48070b775c21d60802e7a72b830b
Jarrod Linahan <jarrod@texh.net>
2014-06-22 03:27:03 +1000

    No longer fails testsuite

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var regex = /((([a-zA-Z]+(-[a-zA-Z]+)?)|\*)(;q=[0-1](\.[0-9]+)?)?)*/g;
 
 
 module.exports.parse = function(al){
-    var strings = (al ? al.toLowerCase() : "").match(regex);
+    var strings = (al || "").match(regex);
     return strings.map(function(m){
         if(!m){
             return;
@@ -14,9 +14,9 @@ module.exports.parse = function(al){
         
         return {
             code:       ietf[0],
-            name:       data.languages[ietf[0]],
+            name:       data.languages[ietf[0].toLowerCase()],
             region:     ietf[1],
-            regionName: data.countries[ietf[1]],
+            regionName: data.countries[ietf[1] ? ietf[1].toLowerCase() : ""],
             quality: bits[1] ? parseFloat(bits[1].split('=')[1]) : 1.0
         };
     }).filter(function(r){
